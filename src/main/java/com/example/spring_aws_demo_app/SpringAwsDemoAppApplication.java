82286e5179a05ad3937d016e296dd0530eb86447
ndhiwar555 <ndhiwar555@gmail.com>
2026-01-30 16:58:57 +0530

    Initial commit

---
+++ src/main/java/com/example/spring_aws_demo_app/SpringAwsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_aws_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/spring_aws_demo_app/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.example.spring_aws_demo_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/api")
+@RestController
+public class AppController {
+
+    @GetMapping(value = "/{name}")
+    public String getMsg(@PathVariable("name") String name) {
+        return "Hello, ".concat(name);
+    }
+}
